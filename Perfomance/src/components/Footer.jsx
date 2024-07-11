57e7ac1c9cc1cc0f9c8d4fb13f1e1a77a3f77f4b
OsipovaES <lizinbonik474@gmail.com>
2024-07-11 18:04:32 +0300

    add: react-components and delete babel.min

---
+++ Perfomance/src/components/Footer.jsx
@@ -0,0 +1,30 @@
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <ul className="footer__list">
+        <li class="footer__item">
+          <a className="footer__link" href="/">
+            Помощь
+          </a>
+        </li>
+        <li className="footer__item">
+          <a className="footer__link" href="/">
+            Обратная связь
+          </a>
+        </li>
+        <li className="footer__item">
+          <a className="footer__link" href="/">
+            Разработчикам
+          </a>
+        </li>
+        <li className="footer__item">
+          <a className="footer__link" href="/">
+            Условия использования
+          </a>
+        </li>
+      </ul>
+
+      <div className="footer__copyright">© 1997–2023 ООО «Яндекс»</div>
+    </footer>
+  );
+};
